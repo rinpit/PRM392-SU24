ce76f85356aa5d5d05e9d76b6d10fb071905150e
Pham Thinh <thinhph9@gmail.com>
2024-05-23 07:17:23 +0700

    slot5_EX6_Activity-Lifecycle

--- PRM392-Exercise/app/src/main/java/com/example/app_demo/MainActivity.java
+++ PRM392-Exercise/app/src/main/java/com/example/app_demo/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.app_demo;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +21,40 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Log.i("MainActivity", "State: Created");
     }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.i("MainActivity", "State: Started");
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.i("MainActivity", "State: Resumed");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.i("MainActivity", "State: Paused");
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.i("MainActivity", "State: Stopped");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.i("MainActivity", "State: Destroy");
+    }
+
+
+
+
 }
